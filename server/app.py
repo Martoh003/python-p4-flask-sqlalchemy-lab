b41e7ea1157833e05b51a6c9f4075d88b1671a64
+ <+>
2023-10-24 10:58:07 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -19,16 +19,65 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter_by(id=id).first()
+    if not animal:
+        response_body = f'This animal is not found'
+        status_code = 404
+        response = make_response(response_body,status_code)
+        return response
+    
+    response_body = f'''<h1>id:{animal.id}</h1> 
+                        <h1>Name: {animal.name}</h1>
+                        <h1>Species: {animal.species}
+                        <h1>Zookeper: {animal.zookeeper.name}</h1>
+                        <h1>Enclosure: {animal.enclosure.environment}</h1>'''
+    response = make_response(response_body,202)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+    if not zookeeper:
+        response_body = 'The zookeeper is not found'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''<h1>id: {zookeeper.id}</h1>
+                        <h1>Name: {zookeeper.name}</h1>
+                        <h1>Birthday: {zookeeper.birthday}</h1>
+                        '''
+    animals = [animal for animal in zookeeper.animals]
+    if not animals:
+        response_body += f'<h1>This zookeper has no animals as of now</h1>'
+    else:
+        for animal in animals:
+            response_body += f'<h1>Animal: {animal.name}</h1>'
+
+    response = make_response(response_body, 202)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter_by(id=id).first()
+    if not enclosure:
+        response_body = 'The zookeeper is not found'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''<h1>id: {enclosure.id}</h1>
+                        <h1>Environment: {enclosure.environment}</h1>
+                        <h1>Open to Visitors: {enclosure.open_to_visitors}</h1>'''
+    
+    animals = [animal for animal in enclosure.animals]
+    if not animals:
+        response_body += f'<h1>This enclosure has no animals as of now</h1>'
+    else:
+        for animal in animals:
+            response_body += f'<h1>Animal: {animal.name}</h1>'
 
+    response = make_response(response_body, 202)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
--- server/models.py
+++ server/models.py
@@ -7,17 +7,36 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
+
+    animals = db.relationship('Animal', back_populates='zookeeper', cascade='all, delete-orphan')
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', back_populates='enclosure', cascade='all, delete-orphan')
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    keeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'), nullable=False)
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'), nullable=False)
+
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
